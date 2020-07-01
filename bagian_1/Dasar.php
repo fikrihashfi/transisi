6655c5d7d731647698316db57a1dd545579a7683
fikrihashfi <fikrihashfi@gmail.com>
2020-07-01 11:46:35 +0700

    highest & lowest

--- bagian_1/Dasar.php
+++ bagian_1/Dasar.php
@@ -4,28 +4,45 @@ class Dasar{
 
     public function mean($nilai){
         $number = explode(" ", $nilai);
-        $sum=0;
+        $sum = 0;
         foreach($number as $n){
             $sum+=$n;
         }
         $mean = $sum/count($number);
-
         return $mean;
     }
 
     public function highest($nilai){
+        $number = explode(" ", $nilai);
+        $highest=0;
+        foreach($number as $n){
+            if($n > $highest){
+                $highest = $n;
+            }
+        }
 
+        return $highest;
     }
 
     public function lowest($nilai){
+        $number = explode(" ", $nilai);
+        if(count($number)>0){
+            $lowest=$number[0];
+        }
+        foreach($number as $n){
+            if($n < $lowest){
+                $lowest = $n;
+            }
+        }
 
+        return $lowest;
     }
 
 }
 
 $dasar = new Dasar();
 $nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
-$dasar->mean($nilai);
+$dasar->lowest($nilai);
 
 
 ?>
