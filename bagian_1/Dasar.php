f58eb0846a60ec08580acf6b4c170e2f31553421
fikrihashfi <fikrihashfi@gmail.com>
2020-07-01 11:41:26 +0700

    mean

--- bagian_1/Dasar.php
+++ bagian_1/Dasar.php
@@ -2,14 +2,30 @@
 
 class Dasar{
 
+    public function mean($nilai){
+        $number = explode(" ", $nilai);
+        $sum=0;
+        foreach($number as $n){
+            $sum+=$n;
+        }
+        $mean = $sum/count($number);
 
+        return $mean;
+    }
 
+    public function highest($nilai){
+
+    }
+
+    public function lowest($nilai){
+
+    }
 
 }
 
 $dasar = new Dasar();
-
-
+$nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
+$dasar->mean($nilai);
 
 
 ?>
